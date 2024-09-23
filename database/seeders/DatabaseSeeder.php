6b06dae0d00e82baa281598f0520b499d114cd62
damar.wiguno <percival5695@gmail.com>
2024-09-23 10:33:09 +0700

    tambahkan data vital

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             DokterSeeder::class,
             ApotekerSeeder::class,
+            TandaVitalSeeder::class,
         ]);
     }
 }
